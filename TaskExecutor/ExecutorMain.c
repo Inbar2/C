ca527c2de89b69d591665fb9b94ab0a8331083c1
Inbar Barhum <95716971+Inbar2@users.noreply.github.com>
2021-12-11 20:48:08 +0200

    Update ExecutorMain.c

--- TaskExecutor/ExecutorMain.c
+++ TaskExecutor/ExecutorMain.c
@@ -1,12 +1,5 @@
-/*/work/advenceC/TaskExecutor$ gcc -g -Wall -ansi -pedantic -I../includeGen -I../projects/LoggerPro ../genericVector/vectorGenericFunc.c ../genericHeap/genHeapFunc.c ExecutorTestsMain.c ExecutorTestsFunctions.c ExecutorTestsHead.h Executor.c ../projects/LoggerPro/zlog4c.c
-*/
-
-
-
 #include "Executor.h"
 #include "zlog4c.h"
-
-/* https://www.draw.io/#G1Bci-Q53G9uXbe_XUibmPM4cwHQEzac-5 */
 
 int main()
 {
